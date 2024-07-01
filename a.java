e4105419a1f821d30d3bc97546b6fa968427e9fd
Aniket Practice <waghmareaniket150@gmail.com>
2024-07-01 22:14:23 +0530

    Added

---
+++ a.java
@@ -0,0 +1,9 @@
+package AniGitHub;
+
+public class a {
+    public static void main(String[] args) {
+        System.out.println("Hey Ani😘");
+        System.out.println("What's up?");
+        
+    }   
+}
